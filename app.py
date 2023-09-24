b12d7de84a62d452614a757d660c2db4a0977f83
Bill <bill@localhost.localdomain>
2023-09-24 14:01:00 -0400

    Scrap pdfs from 'docs' directory

--- app.py
+++ app.py
@@ -1,157 +1,22 @@
-import requests
-import json
 import os
-import html2text
+import glob
 import streamlit as st
 
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlparse
+from web_scraper import get_markdown_from_url, create_index_from_text
+
 from PyPDF2 import PdfReader
 
-from llama_index import Document
-from llama_index import VectorStoreIndex
-from llama_index.node_parser import SimpleNodeParser
-from llama_index.text_splitter import TokenTextSplitter
-
+from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 from langchain.chains import LLMChain
 from langchain.llms import OpenAI, HuggingFaceHub
 from langchain.memory import ConversationBufferMemory
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 from langchain.prompts import PromptTemplate
 
-
 openai_api_key = os.getenv("OPENAI_API_KEY")
-browserless_api_key = os.getenv("BROWSERLESS_API_KEY")
-
-# 1. Scrape raw HTML
-
-def scrape_website(url: str):
-
-    print("Scraping website...")
-    # Define the headers for the request
-    headers = {
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'application/json',
-    }
-
-    # Define the data to be sent in the request
-    data = {
-        "url": url,
-        "elements": [{
-            "selector": "body"
-        }]
-    }
-
-    # Convert Python object to JSON string
-    data_json = json.dumps(data)
-
-    # Send the POST request
-    response = requests.post(
-        f"https://chrome.browserless.io/scrape?token={browserless_api_key}",
-        headers=headers,
-        data=data_json
-    )
-
-    # Check the response status code
-    if response.status_code == 200:
-        # Decode & Load the string as a JSON object
-        result = response.content
-        data_str = result.decode('utf-8')
-        data_dict = json.loads(data_str)
-
-        # Extract the HTML content from the dictionary
-        html_string = data_dict['data'][0]['results'][0]['html']
-
-        return html_string
-    else:
-        print(f"HTTP request failed with status code {response.status_code}")
-
-
-# 2. Convert html to markdown
-
-def convert_html_to_markdown(html):
-
-    # Create an html2text converter
-    converter = html2text.HTML2Text()
-
-    # Configure the converter
-    converter.ignore_links = False
-
-    # Convert the HTML to Markdown
-    markdown = converter.handle(html)
-
-    return markdown
-
-def get_base_url(url):
-    parsed_url = urlparse(url)
-
-    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
-    return base_url
-
-
-# Turn relative url to absolute url in html
-
-def convert_to_absolute_url(html, base_url):
-    soup = BeautifulSoup(html, 'html.parser')
-
-    for img_tag in soup.find_all('img'):
-        if img_tag.get('src'):
-            src = img_tag.get('src')
-            if src and src.startswith(('http://', 'https://')):
-                continue
-            absolute_url = urljoin(base_url, src)
-            img_tag['src'] = absolute_url
-        elif img_tag and img_tag.get('data-src'):
-            src = img_tag.get('data-src')
-            if src and src.startswith(('http://', 'https://')):
-                continue
-            absolute_url = urljoin(base_url, src)
-            img_tag['data-src'] = absolute_url
-
-    for link_tag in soup.find_all('a'):
-        href = link_tag.get('href')
-        #print(href)
-        if href and href.startswith(('http://', 'https://')):
-            continue
-        absolute_url = urljoin(base_url, href)
-        link_tag['href'] = absolute_url
-
-    updated_html = str(soup)
-
-    return updated_html
-
-
-def get_markdown_from_url(url):
-    base_url = get_base_url(url)
-    html = scrape_website(url)
-    updated_html = convert_to_absolute_url(html, base_url)
-    markdown = convert_html_to_markdown(updated_html)
-
-    return markdown
-
-
-# 3. Create vector index from markdown
-
-def create_index_from_text(markdown):
-    text_splitter = TokenTextSplitter(
-        separator="\n",
-        chunk_size=1024,
-        chunk_overlap=20,
-        backup_separators=["\n\n", ".", ","]
-    )
-
-    node_parser = SimpleNodeParser(text_splitter=text_splitter)
-    nodes = node_parser.get_nodes_from_documents(
-        [Document(text=markdown)], show_progress=True)
-
-    # build index
-    index = VectorStoreIndex(nodes)
-
-    print("Index created!")
-    return index
-
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -161,28 +26,9 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
-
-def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-    return chunks
-
-
-def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
-
-
-
+# Main function
 def main():
-
+    
     st.markdown("<div style='text-align:center;'> <img style='width:340px;' src='https://www.ipenclosures.com.au/wp-content/uploads/IP-EnclosuresNZ-Logo-.png.webp' /></div>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align:center;'>IP Enclosures AI Assistant</h1>", unsafe_allow_html=True)
 
@@ -196,21 +42,51 @@ def main():
 
     # Set up the LLMChain, passing in memory
     
-    url = "https://www.ipenclosures.com.au/electrical-enclosures/"
-    query = "Electrical"
-    markdown = get_markdown_from_url(url)
-    index = create_index_from_text(markdown)
+    if "docs_processed" not in st.session_state:
+        st.session_state.docs_processed = False
         
-    # Get relevant data with similarity search
-    retriever = index.as_retriever()
-    nodes = retriever.retrieve(query)
-    texts = [node.node.text for node in nodes]
-    
-    docs = ' '.join(texts)
+    if not st.session_state.docs_processed:
+        
+        docs_directory = os.path.join(os.getcwd(), 'docs')  # Use absolute path to 'docs' directory
+        pdf_files = glob.glob(os.path.join(docs_directory, '*.pdf'))
+            
+        for pdf_file in pdf_files:
+            with open(pdf_file, 'rb') as file:
+                # Perform processing on each PDF file
+                raw_text = get_pdf_text([file])
+                
+                st.session_state.pdf_text = ''.join(raw_text)
+                
+                st.session_state.docs_processed = True
+                
+
+        # REFORMAT !! #
+        ###################################
+        url = "https://www.ipenclosures.com.au/electrical-enclosures/"
+        query = "Electrical"
+        
+        markdown = get_markdown_from_url(url)
+        
+        all_info = st.session_state.pdf_text + markdown
+        
+        index = create_index_from_text(all_info)
+        
+        #############################################
+        
+        # Get relevant data with similarity search
+        retriever = index.as_retriever()
+        
+        # Input query into the retriever
+        nodes = retriever.retrieve(query)
+        
+        texts = [node.node.text for node in nodes]
+        
+        st.session_state.docs = ' '.join(texts)
+        st.session_state.docs_processed = True
     
     template = "Context: "
     
-    template += docs
+    template += st.session_state.docs
     
     template += """You are an AI chatbot for IP Enclosures having a conversation with a human. 
     Please follow the following instructions:
@@ -220,7 +96,6 @@ def main():
     - USE THE CONTEXT PROVIDED TO ANSWER THE USER QUESTION. DO NOT MAKE ANYTHING UP.
     
     - IF RELEVANT, BREAK YOUR ANSWER DO INTO STEPS
-    
     
     - If suitable to the answer, provide any recommendations to products.
     
