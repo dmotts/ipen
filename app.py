335aad709b5a323410a0da92a93d7800b2779d95
Daley <daley.mottley@hotmail.com>
2023-09-25 23:30:50 -0400

    Refactor code add 'Thinking...' loader

--- app.py
+++ app.py
@@ -3,37 +3,66 @@ import glob
 import streamlit as st
 
 from web_scraper import get_markdown_from_url, create_index_from_text
-
-from PyPDF2 import PdfReader
-
+from pdf_scraper import get_pdf_text
 from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import LLMChain
-from langchain.llms import OpenAI, HuggingFaceHub
+from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 from langchain.prompts import PromptTemplate
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
-
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
-
-# Main function
-def main():
-
+    
+def set_up_interface():
     # Set up Streamlit interface
     st.markdown("<div style='text-align:center;'> <img style='width:340px;' src='https://www.ipenclosures.com.au/wp-content/uploads/IP-EnclosuresNZ-Logo-.png.webp' /></div>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align:center;'>IP Enclosures AI Assistant</h1>", unsafe_allow_html=True)
     st.markdown("<p style='text-align:center;'>Welcome to IP Enclosures AI Assistant! Please feel free to ask any question.</p>", unsafe_allow_html=True)
 
+
+def get_prompt_template(docs):
+    template = "Context: "
+    template += st.session_state.docs
+    template += """You are an AI chatbot for IP Enclosures having a conversation with a human. 
+    Please follow the following instructions:
+       
+    - BEFORE ANSWERING THE QUESTION, ASK A FOLLOW UP QUESTION.
+    
+    - USE THE CONTEXT PROVIDED TO ANSWER THE USER QUESTION. DO NOT MAKE ANYTHING UP.
+    
+    - IF RELEVANT, BREAK YOUR ANSWER DO INTO STEPS
+    
+    - If suitable to the answer, provide any recommendations to products.
+    
+    - FORMAT YOUR ANSWER IN MARKDOWN
+    
+    - ALWAYS ASK FOLLOW UP QUESTIONS!
+
+    - SHOW RELEVANT IMAGES OF PRODUCTS
+
+    - DO NOT MAKE ANYTHING UP            
+    
+    {history}
+    Human: {human_input}
+    AI: """
+
+    return template
+
+def generate_response(query, memory, docs):
+    template = get_prompt_template(docs)
+    prompt = PromptTemplate(input_variables=["history", "human_input"], template=template)
+    llm_chain = LLMChain(llm=OpenAI(openai_api_key=openai_api_key), prompt=prompt, memory=memory)
+    return llm_chain.run(query)
+
+# Main function
+def main():
+    # Initialize website url        
+    url = "https://www.ipenclosures.com.au/electrical-enclosures/"
+
+    # Set up Streamlit interface
+    set_up_interface()
+    
     # Set up memory
     msgs = StreamlitChatMessageHistory(key="langchain_messages")
     memory = ConversationBufferMemory(chat_memory=msgs)
@@ -42,98 +71,59 @@ def main():
     if len(msgs.messages) == 0:
         # Display initial message only at the very start
         st.chat_message("ai").write("How can I help you?")  # AI's initial message
+    
+    if query:= st.chat_input("Your message"):       
+        print(f'Query: {query}')
 
-    # Initialize query_input as None
-    query_input = None
+        # Render current messages from StreamlitChatMessageHistory
+        for msg in msgs.messages:
+            st.chat_message(msg.type).write(msg.content)
 
-    # Get user input for query
-    query_input = st.chat_input("Your message")  # Unique key for query input
+        st.chat_message("human").write(query)
 
-    # Display user's query in the interface if query_input is set
-    if query_input:
-       # Process PDF documents
-        docs_directory = os.path.join(os.getcwd(), 'docs')  # Use absolute path to 'docs' directory
-        pdf_files = glob.glob(os.path.join(docs_directory, '*.pdf'))
+        with st.chat_message('ai'):
+            with st.spinner('Thinking...'):
+                # Process PDF documents
+                docs_directory = os.path.join(os.getcwd(), 'docs')  # Use absolute path to 'docs' directory
+                pdf_files = glob.glob(os.path.join(docs_directory, '*.pdf'))
 
-        if "docs_processed" not in st.session_state:
-            st.session_state.docs_processed = False
-            
-        if not st.session_state.docs_processed:
-            for pdf_file in pdf_files:
-                with open(pdf_file, 'rb') as file:
-                    # Perform processing on each PDF file
-                    raw_text = get_pdf_text([file])
+                if "docs_processed" not in st.session_state:
+                    st.session_state.docs_processed = False
+                    
+                if not st.session_state.docs_processed:
+                    for pdf_file in pdf_files:
+                        with open(pdf_file, 'rb') as file:
+                            # Perform processing on each PDF file
+                            raw_text = get_pdf_text([file])
 
-                    st.session_state.pdf_text = ''.join(raw_text)
-                    st.session_state.docs_processed = True 
-        
-        url = "https://www.ipenclosures.com.au/electrical-enclosures/"
+                            st.session_state.pdf_text = ''.join(raw_text)
+                            st.session_state.docs_processed = True 
 
-        # Scrap website url and retrieve markdown
-        markdown = get_markdown_from_url(url)
+                # Scrap website url and retrieve markdown
+                markdown = get_markdown_from_url(url)
 
-        # Combine pdf_text and markdown
-        all_info = st.session_state.pdf_text + markdown
+                # Combine pdf_text and markdown
+                all_info = st.session_state.pdf_text + markdown
 
-        index = create_index_from_text(all_info)
+                index = create_index_from_text(all_info)
 
-        # Get relevant data with similarity search
-        retriever = index.as_retriever()
-
-        print(f'Query: {query_input}')
-
-        # Input query into the retriever
-        nodes = retriever.retrieve(query_input)
-
-        texts = [node.node.text for node in nodes]
-
-        st.session_state.docs = ' '.join(texts)
-
-        # Check if a query is provided before generating a prompt
-        if query_input:
-            template = "Context: "
-            template += st.session_state.docs
-            template += """You are an AI chatbot for IP Enclosures having a conversation with a human. 
-            Please follow the following instructions:
-               
-            - BEFORE ANSWERING THE QUESTION, ASK A FOLLOW UP QUESTION.
-            
-            - USE THE CONTEXT PROVIDED TO ANSWER THE USER QUESTION. DO NOT MAKE ANYTHING UP.
-            
-            - IF RELEVANT, BREAK YOUR ANSWER DO INTO STEPS
-            
-            - If suitable to the answer, provide any recommendations to products.
-            
-            - FORMAT YOUR ANSWER IN MARKDOWN
-            
-            - ALWAYS ASK FOLLOW UP QUESTIONS!
-
-            - SHOW RELEVANT IMAGES OF PRODUCTS
-
-            - DO NOT MAKE ANYTHING UP            
-            
-            {history}
-            Human: {human_input}
-            AI: """
-
-            prompt = PromptTemplate(input_variables=["history", "human_input"], template=template)
-            llm_chain = LLMChain(llm=OpenAI(openai_api_key=openai_api_key), prompt=prompt, memory=memory)
-
-            # Render current messages from StreamlitChatMessageHistory
-            for msg in msgs.messages:
-                st.chat_message(msg.type).write(msg.content)
-
-            # If user inputs a new prompt, generate and draw a new response
-            if query_input:
+                # Get relevant data with similarity search
+                retriever = index.as_retriever()
                 
-                st.chat_message("human").write(query_input)
-        
-                # Note: new messages are saved to history automatically by Langchain during run
-                response = llm_chain.run(query_input)
+                # Input query into the retriever
+                nodes = retriever.retrieve(query)
 
+                texts = [node.node.text for node in nodes]
+
+                st.session_state.docs = ' '.join(texts)
+                
+                docs = st.session_state.docs
+                
+                # Note: new messages are saved to history automatically by LangChain during run
+                response = generate_response(query, memory, docs)
                 print(f'Response: {response}')
 
-                st.chat_message("ai").write(response)
+                st.write(response)
 
 if __name__ == '__main__':
     main()
